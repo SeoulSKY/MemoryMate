ad268fa79607607336d4e272ebfc28c312984495
SeoulSKY <email@seoulsky.org>
2024-04-07 18:17:45 -0600

    Setup dotenv

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 
 export default function App () {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Your GEMINI AI Token: {process.env.EXPO_PUBLIC_GEMINI_TOKEN}</Text>
       <StatusBar style="auto" />
     </View>
   );
